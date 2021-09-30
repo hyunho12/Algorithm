f294d13d34779e8058dba9ddf8fcfb3ab87f5f10
hyundho12 <hyundho12@gmail.com>
2021-09-30 14:15:37 +0900

    시간초과 해결

--- 11724/11724/_11724.py
+++ 11724/11724/_11724.py
@@ -1,9 +1,29 @@
-def cal(n):
-    if n == 0:
-        return 0
-    else:
-        return n+cal(n-1)
+from collections import deque
 
-n = int(input())
-print(cal(n))
-print("git Test")
+n,m = map(int,input().split())
+
+arr = [[] for _ in range(n+1)]
+visit = [0] * (n+1)
+res = 0
+
+def bfs(a):
+    q = deque()
+    q.append(a)
+
+    while q:
+        v = q.popleft()
+        if visit[v] == 0:
+            visit[v] = 1
+            for i in arr[v]:
+                q.append(i)
+
+for i in range(m):
+    u,v = map(int,input().split())
+    arr[u].append(v)
+    arr[v].append(u)
+
+for i in range(1,n+1):    
+    if visit[i] == 0:
+        bfs(i)
+        res += 1
+print(res)
